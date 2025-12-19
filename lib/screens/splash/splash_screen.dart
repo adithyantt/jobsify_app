a2c03ffa86bafe2626f401482030f30ed1cff381
Adithyan T T <adithyantt00@gmail.com>
2025-12-19 11:16:34 +0530

    this is the splash screen

---
+++ lib/screens/splash/splash_screen.dart
@@ -0,0 +1,53 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import '../auth/login_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const LoginScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Icon(Icons.work, size: 80, color: Colors.white),
+            SizedBox(height: 20),
+            Text(
+              "Jobsify",
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: 8),
+            Text(
+              "Find Local Jobs Easily",
+              style: TextStyle(color: Colors.white70),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
