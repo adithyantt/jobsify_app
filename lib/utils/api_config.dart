06de0e0d88ea3c75d0ba45e05dcb934b05046dd5
Adithyan T T <adithyantt00@gmail.com>
2026-03-16 23:44:11 +0530

    adding this file while trying to modify the UI

--- lib/utils/api_config.dart
+++ lib/utils/api_config.dart
@@ -1,4 +1,4 @@
 class ApiConfig {
-  // Using local server IP - for physical device testing
-  static const String baseUrl = "http://10.77.250.105:8000";
+  // Fixed for both emulator & physical device
+  static const String baseUrl = "http://10.253.86.105:8000";
 }
